410561ddf96d820381acdf0ca0829398a6188392
ashwinisingan <ashwinisingan@gmail.com>
2017-06-22 11:26:11 -0500

    Update async.js

--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+     
+    var dfd = $.Deferred();
+      setTimeout(function() { dfd.resolve(value);  }, 10);
+      return dfd.promise();
+ 
   },
 
   manipulateRemoteData: function(url) {
 
+      var dfd = $.Deferred();
+
+    $.ajax(url).then(function(resp) {
+      var name = $.map(resp.name, function(names) {
+        return names.name;
+      });
+      dfd.resolve(name.sort());
+    });
+
+    return dfd.promise();
   }
 };
