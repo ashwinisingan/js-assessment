b832e666ddc47d31f11273d95bc2201dc172794e
ashwinisingan <ashwinisingan@gmail.com>
2017-06-22 11:28:34 -0500

    Update count.js

--- app/count.js
+++ app/count.js
@@ -1,7 +1,15 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+    count: function (start, end) {
 
-  }
+        var timend;
+        function doIt() {
+            console.log(start++);
+
+            if (start <= end) {
+                timend = setTimeout(doIt, 100);
+            }
+        }
+    }
 };
