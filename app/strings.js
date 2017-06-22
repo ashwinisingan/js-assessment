6d8e552e3c4d34518d314427c26e2ad20581afe0
ashwinisingan <ashwinisingan@gmail.com>
2017-06-22 11:36:37 -0500

    Update strings.js

--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,58 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+      var count = 0;
+      var redstring = '';
+      for (var pos = 0; pos < str.length; pos++) {
+          if (str[pos] === str[pos + 1]) {
+              count++;
+              if (count < amount) {
+                  redstring += str[pos];
+              }
+          } else {
+              count = 0;
+              redstring += str[pos];
+          }
+      };
+      return redstring;
   },
 
   wordWrap: function(str, cols) {
+     
+      var formatedString = '',
+       Arrayofwords = [];
+
+
+      Arrayofwords = str.split(' ');
+
+      formatedString = Arrayofwords[0];
+
+      for (var i = 1; i < Arrayofwords.length; i++)
+      {
+          if (Arrayofwords[i].length > cols)
+          {
+              formatedString += '\n' + Arrayofwords[i];
+          }
+          else
+          {
+              if (formatedString.length + Arrayofwordsy[i].length > cols)
+              {
+                  formatedString += '\n' + Arrayofwords[i];
+              }
+              else
+              {
+                  formatedString += ' ' + Arrayofwords[i];
+              }
+          }
+      }
+
+      console.log(formatedString);
 
   },
 
   reverseString: function(str) {
+      return str.split('').reverse().join('');
+
 
   }
 };
